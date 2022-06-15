298be47ed4ebb07b3a26e3a1a6bcf4d783fba26b
Oleh Lamzin <olehtomas@gmail.com>
2022-06-15 21:52:41 +0200

    Add monobank types and accounts

--- src/donate-monitor/src/app/app.module.ts
+++ src/donate-monitor/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
 import { MonobankComponent } from './monobank/monobank.component';
 
 @NgModule({
@@ -22,11 +25,13 @@ import { MonobankComponent } from './monobank/monobank.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/donate-monitor/src/app/monobank/monobank.component.ts
+++ src/donate-monitor/src/app/monobank/monobank.component.ts
@@ -10,6 +10,11 @@ import { FormControl } from '@angular/forms';
 export class MonobankComponent implements OnInit {
   token = new FormControl('');
 
+  clientInfo: any;
+  selectedEntity: AccountEntity | null = null;
+
+  cardsAndJars: Array<AccountEntity> = [];
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -26,7 +31,47 @@ export class MonobankComponent implements OnInit {
 
     response.subscribe(response => {
       console.log("Response from server: ", response);
+      this.clientInfo = response;
+ 
+      this.cardsAndJars = this.calculateCardsAndJars(this.clientInfo);
     });
-
   }
-}
+
+  private calculateCardsAndJars(clientInfo: any): Array<AccountEntity> {
+    let arr: Array<AccountEntity> = [];
+    clientInfo.accounts.forEach((element: any) => {
+      arr.push(new AccountEntity(element.id, element.currencyCode));
+    });
+    return arr;
+  }
+
+
+};
+
+export class AccountEntity {
+  id: string;
+  currencyCode: number;
+  value: string = 'some value';
+
+  constructor(id: string, currencyCode: number, ) {
+    this.id = id;
+    this.currencyCode = currencyCode;
+  }
+
+  public toString(): string {
+    return this.id;
+  }
+
+};
+
+
+// clientId: '45JkoMSKNk',
+// name: 'Ламзін Олег',
+// webHookUrl: '',
+// permissions: 'psfj',
+// accounts: Array(3), …}
+// accounts: Array(3)
+// 0: { id: 'c4sTeBeEoyDQGXpV4PO9Vg', sendId: '45JkoMSKNk', currencyCode: 980, cashbackType: 'UAH', balance: 5287604, … } 
+// 1: { id: '8jhGhDeBx_8sIRxZltZMKA', sendId: '6XcS5yGBH7', currencyCode: 840, cashbackType: 'UAH', balance: 0, … } 
+// 2: { id: '6ibWwkOD45VPQEQC_Q9omA', sendId: '6Us9GQgxF5', currencyCode: 978, cashbackType: 'UAH', balance: 16684, … } length: 3[[Prototype]]: Array(0)clientId: "45JkoMSKNk"
+// jars: (2)[{… }, {… }]name: "Ламзін Олег"permissions: "psfj"webHookUrl: ""[[Prototype]]: Object
