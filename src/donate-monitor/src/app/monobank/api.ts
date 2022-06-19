1c1ab316b935d6254aa0d60c7b266840284c91a7
Oleh Lamzin <olehtomas@gmail.com>
2022-06-19 23:20:47 +0200

    Add more account info

--- src/donate-monitor/src/app/monobank/api.ts
+++ src/donate-monitor/src/app/monobank/api.ts
@@ -41,12 +41,12 @@ export class MonoApi {
             response.subscribe(response => {
                 console.log("Response from server: ", response);
                 let transactions = new Array<Transaction>();
-                
+
                 if (!Array.isArray(response)) {
                     reject("Error happened: " + response);
                     return;
                 }
-                
+
                 response.forEach(item => {
                     transactions.push(new Transaction(item));
                 });
@@ -85,38 +85,76 @@ export class ClientInfo {
 
 export class Account implements AccountEntity {
     id: string;
+    sendId: string;
+    balance: number;
+    currencyCode: number;
     maskedPan: string;
 
     constructor(o: any) {
         this.id = o.id;
+        this.sendId = o.sendId;
+        this.balance = o.balance;
+        this.currencyCode = o.currencyCode;
         this.maskedPan = o.maskedPan[0];
     }
 
-    value(): string {
+    getShortDescription(): string {
         return this.maskedPan;
+    }
+    getUrl() : string {
+        return "";
+    }
+    getLongDescription(): string {
+        return "";
+    }
+    getBalance(): string {
+        return formatMoney(this.balance, this.currencyCode);
     }
 }
 
 export class Jar implements AccountEntity {
     id: string;
+    sendId: string;
     title: string;
     description: string;
+    currencyCode: number;
+    balance: number;
+    goal: number;
 
     constructor(o: any) {
         this.id = o.id;
+        this.sendId = o.sendId;
         this.title = o.title;
         this.description = o.description;
+        this.currencyCode = o.currencyCode;
+        this.balance = o.balance;
+        this.goal = o.goal;
     }
 
-    value(): string {
+    getShortDescription(): string {
         return this.title;
+    }
+    getUrl() : string {
+        return `https://send.monobank.ua/${this.sendId}`;
+    }
+    getLongDescription(): string {
+        return this.description;
+    }
+    getBalance(): string {
+        if (!this.goal) {
+            return formatMoney(this.balance, this.currencyCode);
+        }
+        return `${formatMoney(this.balance, this.currencyCode)} ouf ot ${formatMoney(this.goal, this.currencyCode)}`;
     }
 }
 
 export interface AccountEntity {
     id: string;
 
-    value(): string;
+    getShortDescription(): string;
+    getUrl(): string;
+    getLongDescription(): string;
+    getBalance(): string;
 }
 
 export class Transaction {
@@ -128,5 +166,35 @@ export class Transaction {
         this.time = o.time;
         this.description = o.description;
         this.amount = o.amount;
+    }
+}
+
+function formatMoney(money: number, currencyCode: number): string {
+    let s = `${(money / 100).toFixed(0)}`;
+    let m = "";
+    for (let i = 0; i < s.length; i++) {
+        if (i > 0 && (s.length - i) % 3 == 0) {
+            m += " ";
+        }
+        m += s[i];
+    }
+
+    return `${m}${currencySymbol(currencyCode)}`;
+}
+
+function currencySymbol(currencyCode: number): string {
+    switch (currencyCode) {
+        case 840: {
+            return "$";
+        }
+        case 978: {
+            return "€";
+        }
+        case 980: {
+            return "₴";
+        }
+        default: {
+            return ` "${currencyCode}"`;
+        }
     }
 }
